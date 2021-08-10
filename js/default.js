af5ec05fc248fdfc60798a54a379de11a04d7e79
mohamed khounti <30549055+larbikhounti@users.noreply.github.com>
2021-08-10 07:02:07 -0700

    files

---
+++ js/default.js
@@ -0,0 +1,30 @@
+
+// get refercences
+var player = document.querySelector("lottie-player");
+// on scroll
+let scrollval = 0;
+window.addEventListener("scroll", () => {
+    if (scrollval > window.scrollY) {
+        //  // change direction to backwards
+        player.setDirection(-1);
+        player.play();
+
+    } else if(scrollval < window.scrollY) {
+        // change direction to upwards
+        player.setDirection(1);
+        player.play();
+    }
+    scrollval = window.scrollY;
+});
+
+// if stops scrolling
+var timer = null;
+window.addEventListener('scroll', function() {
+    if(timer !== null) {
+        clearTimeout(timer);        
+    }
+    timer = setTimeout(function() {
+        // if stops scrolling pause
+        player.pause();
+    }, 0);
+}, false);
